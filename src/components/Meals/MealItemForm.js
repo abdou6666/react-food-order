c7252ce42571c9e6fa5b861505789adb8034c100
abdou sfayhi <abdousfayhi12@gmail.com>
2021-08-30 21:49:14 +0100

    food order app first commit

---
+++ src/components/Meals/MealItemForm.js
@@ -0,0 +1,10 @@
+import classes from "MealItemForm.module.css"
+const MealItemForm = () => {
+    return (
+        <form className={classes.form}>
+            <button className={classses.button}></button>
+        </form>
+    )
+}
+
+export default MealItemForm
